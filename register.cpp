55822c7ba37112efc36b46ca12f9363ee4cf5e47
Tyla Jeffs <tylajeffs@gmail.com>
2022-09-28 23:09:13 +0000

    fix the rest of the bugs, complete user interactions

--- register.cpp
+++ register.cpp
@@ -16,7 +16,6 @@ int Register::takeCash(double amountOwed, double amountPaid, Dispenser& d) {
     
     //calculate the change
     double leftover = amountOwed - amountPaid;
-    cout << "leftover: "<< leftover << endl;
     
     
     //see if they paid the right amount
@@ -30,25 +29,32 @@ int Register::takeCash(double amountOwed, double amountPaid, Dispenser& d) {
         cash += amountOwed;
         cout << "The register has $" << cash << endl;
         
-    } else if(leftover > 0) { //they paid too much, need to give change
+    } else if(leftover > 0) { //they didn't pay enough, ask for more
     
-        cout << "Returning $" << leftover << endl;
+        //add the payment to total cash
+        cash += amountPaid;
+    
+        double num;
+        cout << "Insert $" << leftover << ": " ;
+        cin >> num;
+        
+        //call take cash again
+        takeCash(leftover, num, d);
+    
+        
+    } else { //they overpaid, give back change
+    
+    
+        cout << "Returning $" << leftover * -1 << endl;
+        
+        //subtract one from the correct dispenser
+        d.dispensed();
+        cout << d.getItemType()<< " dispensed! " << endl;
         
         //add the payment to total cash and print
         cash += amountOwed;
         cout << "The register has $" << cash << endl;
         
-    } else { //they didn't pay enough, ask for more $
-        
-        //add the payment to total cash
-        cash += amountPaid;
-    
-        double num;
-        cout << "Insert $" << leftover * -1 << ": " << endl;
-        cin >> num;
-        
-        //call take cash again
-        takeCash(leftover * -1, num, d);
     }
     
     
